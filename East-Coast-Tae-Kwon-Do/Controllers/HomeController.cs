4a399c300bb65ea8ec68b57f3ef8f283bd64e8aa
Kaleb <kjwrigley08@gmail.com>
2024-03-24 16:49:43 -0500

    Initial repository

---
+++ East-Coast-Tae-Kwon-Do/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace East_Coast_Tae_Kwon_Do.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        string userAgent = HttpContext.Request.Headers["User-Agent"];
+        bool isMobile = userAgent.Contains("Android") || userAgent.Contains("Mobile") || userAgent.Contains("iPhone");
+        return View();
+
+        ViewBag.IsMobile = isMobile;
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+}
